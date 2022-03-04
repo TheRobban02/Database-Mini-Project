cd19778e2b6e357ef915e8b6da615a987ac24b92
Erik Borgström <eb223fe@student.lnu.se>
2022-03-04 10:33:02 +0100

    upd

--- Database.py
+++ Database.py
@@ -1,8 +1,6 @@
 # Import mysql connector, pandas, numpy and msvcrt
-import msvcrt
 import mysql.connector as mysql
 import pandas as pd
-import numpy as np
 
 # Connect to SQL Server
 connection = mysql.connect(
@@ -12,102 +10,6 @@ connection = mysql.connect(
     passwd = "root"
 
 )
-
-# Main Menu
-def MainMenu():
-
-    cursor = connection.cursor(buffered=True)
-    cursor.execute(f"USE {DB_NAME}")
-
-    choice = input("\n1. List all planets\
-        \n2. Search for planet details\
-        \n3. Search for species with height higher than given number\
-        \n4. What is the most likely desired climate of the given species?\
-        \n5. What is the average lifespan per species classification?\
-        \nQ: Quit\
-        \n--------\
-        \nChoice:  ")
-
-    if (choice == "1"):
-        
-        cursor.execute("SELECT name FROM Planets")
-        records = cursor.fetchall()
-        
-        for row in records:
-            print(row[0])
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "2"):
-
-        planetName = input("Enter the name of the planet: ")
-        cursor.execute(f"SELECT * FROM Planets WHERE name = '{planetName}'")
-        records = cursor.fetchall()
-
-        for row in records:
-            for i in row:
-                print(i)
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "3"):
-
-        height = int(input("Enter the average height of the species: "))
-        cursor.execute(f"SELECT name FROM Species WHERE average_height > {height}")
-        records = cursor.fetchall()
-
-        for row in records:
-            print(row[0])
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "4"):
-
-        specie = input("Enter the name of the specie: ")
-        cursor.execute(f"SELECT climate FROM Planets, Species WHERE Species.name = '{specie}' and Species.homeworld = Planets.name")
-
-        records = cursor.fetchall()
-
-        for row in records:
-            print(f"Desired climate: {row[0]}")
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "5"):
-        cursor.execute(f"SELECT classification, ROUND(AVG(average_lifespan), 1) FROM Species WHERE average_lifespan != '0' and classification != '0' GROUP BY classification")
-        records = cursor.fetchall()
-
-        for row in records:
-            if(row[1]) == 1010:
-                print(f"Classification: {row[0]}, Average Lifespan: Indefinite")
-            else:
-                print(f"Classification: {row[0]}, Average Lifespan: {row[1]}")
-
-        print("\nPress any key to return to main menu: ")
-        
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    else:
-        exit()
-
 def CreateDatabes():
 
     cursor = connection.cursor(buffered=True)
@@ -147,8 +49,6 @@ def CreateDatabes():
         
         #save our changes
         connection.commit()
-    
-    MainMenu()
 
 DB_NAME = "starCitizen"
 
@@ -160,10 +60,6 @@ dataShips = pd.read_csv(r'SpaceStations.csv', delimiter=",")
 
 # Removes row in planets where name is NaN
 dataP.dropna(subset = ["name"], inplace=True)
-
-# Replaces NaN with 0
-dataP = dataP.replace(np.nan, 0)
-dataS = dataS.replace(np.nan, 0)
 
 # Replaces indefinite with 1010 to easier handle it
 dataS["average_lifespan"].replace({"indefinite": 1010}, inplace=True)
