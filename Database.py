9ca1970bc18ae1794def474abdc3b1f378bafb06
Erik Borgström <eb223fe@student.lnu.se>
2022-03-04 11:19:09 +0100

    upd

--- Database.py
+++ Database.py
@@ -10,173 +10,104 @@ connection = mysql.connect(
     passwd = "root"
 
 )
-<<<<<<< HEAD
-=======
 
-# Main Menu
-def MainMenu():
+DB_NAME = "starCitizen"
 
-    cursor = connection.cursor(buffered=True)
-    cursor.execute(f"USE {DB_NAME}")
-
-    choice = input("\n1. List all planets\
-        \n2. Search for planet details\
-        \n3. Search for species with height higher than given number\
-        \n4. What is the most likely desired climate of the given species?\
-        \n5. What is the average lifespan per species classification?\
-        \nQ: Quit\
-        \n--------\
-        \nChoice:  ")
-
-    if (choice == "1"):
-        
-        cursor.execute("SELECT name FROM Planets")
-        records = cursor.fetchall()
-        
-        for row in records:
-            print(row[0])
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "2"):
-
-        planetName = input("Enter the name of the planet: ")
-        cursor.execute(f"SELECT * FROM Planets WHERE name = '{planetName}'")
-        records = cursor.fetchall()
-
-        for row in records:
-            for i in row:
-                print(i)
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "3"):
-
-        height = int(input("Enter the average height of the species: "))
-        cursor.execute(f"SELECT name FROM Species WHERE average_height > {height}")
-        records = cursor.fetchall()
-
-        for row in records:
-            print(row[0])
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "4"):
-
-        specie = input("Enter the name of the specie: ")
-        cursor.execute(f"SELECT climate FROM Planets, Species WHERE Species.name = '{specie}' and Species.homeworld = Planets.name")
-
-        records = cursor.fetchall()
-
-        for row in records:
-            print(f"Desired climate: {row[0]}")
-        
-        print("\nPress any key to return to main menu: ")
-
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    elif (choice == "5"):
-        cursor.execute(f"SELECT classification, ROUND(AVG(average_lifespan), 1) FROM Species WHERE average_lifespan != '0' and classification != '0' GROUP BY classification")
-        records = cursor.fetchall()
-
-        for row in records:
-            if(row[1]) == 1010:
-                print(f"Classification: {row[0]}, Average Lifespan: Indefinite")
-            else:
-                print(f"Classification: {row[0]}, Average Lifespan: {row[1]}")
-
-        print("\nPress any key to return to main menu: ")
-        
-        # Listens to any keypress
-        if(msvcrt.getch()):
-            MainMenu()
-
-    else:
-        exit()
-
-# This will need to change
->>>>>>> e1f81e4a5fce225af1ed064dfec650d249b12130
-def CreateDatabes():
+def CreateDatabase():
 
     cursor = connection.cursor(buffered=True)
 
     # Create the database
-    cursor.execute("create database {}".format(DB_NAME)) 
+    cursor.execute("create database {}".format(DB_NAME))
     
     cursor.execute("select database();")
     
     # Create Table
     cursor.execute(f"USE {DB_NAME}")
-    cursor.execute("CREATE TABLE Planets (name nvarchar(50) not null, rotation_period long, orbital_period long, \
-        diameter long, climate nvarchar(100), gravity nvarchar(100), terrain nvarchar(100), surface_water long, population long)")
+    cursor.execute("CREATE TABLE Planets (name nvarchar(50) not null primary key, capital nvarchar(50), system nvarchar(20),\
+        moons nvarchar(100), space_station nvarchar(50), minTemp int, maxTemp int))")
+    
+    # Create Table
+    cursor.execute(f"USE {DB_NAME}")
+    cursor.execute("CREATE TABLE Species (name varchar(50) not null primary key, sentient boolean, language nvarchar(20),\
+        goverment nvarchar(100))")
+
+    cursor.execute(f"USE {DB_NAME}")
+    cursor.execute("CREATE TABLE Ships (name nvarchar(50) not null primary key, manufacturer nvarchar(60), price int,\
+        buy_location nvarchar(50), role nvarchar(50), cargo_capacity_(SCU) int, lenght float, race nvarchar(20))")
 
     # Create Table
     cursor.execute(f"USE {DB_NAME}")
-    cursor.execute("CREATE TABLE Species (name varchar(50) not null, classification nvarchar(50), designation nvarchar(20), \
-        average_height int, skin_colors nvarchar(100), hair_colors nvarchar(100), eye_colors nvarchar(100), average_lifespan int, \
-             language nvarchar(100), homeworld nvarchar(50))")
+    cursor.execute("CREATE TABLE Weapons (name nvarchar(50) not null primary key, type nvarchar(50), size int,\
+        dps int, fire int, buy_location nvarchar(50), price int))")
 
-    #loop through the data frame
-    for i,row in dataP.iterrows():
-        
+    cursor.execute(f"USE {DB_NAME}")
+    cursor.execute("CREATE TABLE Stations (station_id nvarchar(50) not null primary key, planet nvarchar(50), refinery boolean,\
+        black_market boolean, hangars int, pads int, docking_ports int, name nvarchar(50), system nvarchar20))")
+
+    PopulateTables()
+
+
+def PopulateTables():
+    
+    #loop through the data frames
+    data = pd.read_csv (r'planets.csv', delimiter=";")
+    for i,row in data.iterrows():
+            
         #here %S means string values 
-        sql = f"INSERT INTO {DB_NAME}.planets VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        sql = f"INSERT INTO {DB_NAME}.planets VALUES (%s,%s,%s,%s,%s,%s,%s)"
         cursor.execute(sql, tuple(row))
-        
+            
         #save our changes
         connection.commit()
     
     #loop through the data frame
-    for i,row in dataS.iterrows():
+    data = pd.read_csv (r'species.csv', delimiter=";")
+    for i,row in data.iterrows():
         
         #here %S means string values 
-        sql = f"INSERT INTO {DB_NAME}.species VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        sql = f"INSERT INTO {DB_NAME}.species VALUES (%s,%s,%s,%s)"
         cursor.execute(sql, tuple(row))
         
         #save our changes
         connection.commit()
 
-DB_NAME = "starCitizen"
+    #loop through the data frames
+    data = pd.read_csv(r'ships.csv', delimiter=";")
+    for i,row in data.iterrows():
+            
+        #here %S means string values 
+        sql = f"INSERT INTO {DB_NAME}.ships VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, tuple(row))
+            
+        #save our changes
+        connection.commit()
+    
+    #loop through the data frames
+    data = pd.read_csv(r'weapons.csv', delimiter=";")
+    for i,row in data.iterrows():
+            
+        #here %S means string values 
+        sql = f"INSERT INTO {DB_NAME}.weapons VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, tuple(row))
+            
+        #save our changes
+        connection.commit()
+    
+    #loop through the data frames
+    data = pd.read_csv(r'spacestation.cvs', delimiter=";")
+    for i,row in data.iterrows():
+            
+        #here %S means string values 
+        sql = f"INSERT INTO {DB_NAME}.stations VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql, tuple(row))
+            
+        #save our changes
+        connection.commit()
 
-# Pandas reads csv file and creates data frames dataP and dataS
-dataPlanets = pd.read_csv (r'planets.csv', delimiter=",")
-dataSpecies = pd.read_csv (r'species.csv', delimiter=",")
-dataShips = pd.read_csv(r'SpaceStations.csv', delimiter=",")
-# TODO dataWeapons = pd.read_csv(r'')
 
-<<<<<<< HEAD
-# Removes row in planets where name is NaN
-dataP.dropna(subset = ["name"], inplace=True)
-
-# Replaces indefinite with 1010 to easier handle it
-dataS["average_lifespan"].replace({"indefinite": 1010}, inplace=True)
-
-# Change reptilian to reptile and mammals to mammal
-dataS["classification"].replace({"reptilian": "reptile"}, inplace=True)
-dataS["classification"].replace({"mammals": "mammal"}, inplace=True)
-
-=======
->>>>>>> e1f81e4a5fce225af1ed064dfec650d249b12130
 cursor = connection.cursor()
 cursor.execute("show databases")
 lst = cursor.fetchall()
 
-# Checks if Database exists
-if(DB_NAME,) in lst:
-    MainMenu()
-else:
-    CreateDatabes()
+CreateDatabase()
